2a97f59ec547c4b5b03c294f90d88f3fd722b7b8
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-01 08:08:58 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,6 +5,7 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
+    
 ഞാൻ 🛡️<a href="https://t.me/cineblasters">CɪNᴇ BʟAsTᴇRs</a>🛡️ എന്ന ഗ്രൂപ്പിൽ മൂവീസ് കൊടുക്കുന്ന ബോട്ട് ആണ്....😉
 
 നോക്കണ്ടാ എന്നെ മറ്റു ഗ്രൂപ്പിൽ ഒന്നും ഉപയോഗിക്കാൻ കഴിയുകയില്ല..😌
@@ -19,13 +20,5 @@ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ <a href="https://t.me/peace_fighter_TG">Tʜɪs Pᴇ
 
 
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+    ABOUT_TEXT = """<b>➥ Name</b> : <b>നീ ഏതാ..... ഒന്ന് പോടെയ്</b>
 """
